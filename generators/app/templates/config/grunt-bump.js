4b6ffa2e5bd989fa46e3d337a5024f54c31fcb1d
Corey Ferguson <corey.t.ferguson@gmail.com>
2016-05-29 15:53:46 -0700

    Grunt bump to push automatically.

--- generators/app/templates/config/grunt-bump.js
+++ generators/app/templates/config/grunt-bump.js
@@ -1,6 +1,20 @@
 
 module.exports = {
   options: {
-    push: false
+    files: ['package.json'],
+    updateConfigs: [],
+    commit: true,
+    commitMessage: 'Release v%VERSION%',
+    commitFiles: ['package.json'],
+    createTag: true,
+    tagName: 'v%VERSION%',
+    tagMessage: 'Version %VERSION%',
+    push: true,
+    pushTo: 'origin',
+    gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+    globalReplace: false,
+    prereleaseName: false,
+    metadata: '',
+    regExp: false
   }
 };
